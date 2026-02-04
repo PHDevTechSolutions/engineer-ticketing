84909645938962bbd5b0e670f90fe755efd7d112
PHDevTechSolutions <phdevtechsolutions@gmail.com>
2026-02-05 00:27:38 +0800

    louies

--- utils/supabase-ticket.js
+++ utils/supabase-ticket.js
@@ -4,4 +4,8 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL_TICKET;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+  supabaseUrl || 'https://placeholder.supabase.co', 
+  supabaseKey || 'placeholder'
+);
+
